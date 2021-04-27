4ebb35c20a39adc8d13f8ea2fc0fefa930b769d0
randy <randy@test-cube.com>
2021-04-27 21:40:05 +0700

    fix css

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,4 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-    .less('public/css/footer.less', 'public/css')
-    .less('public/css/header.less', 'public/css')
-    .less('public/css/response.less', 'public/css')
-    .less('public/css/content.less', 'public/css')
-    .less('public/css/admin.less', 'public/css')
-    .less('public/css/blog.less', 'public/css')
     .less('public/css/index.less', 'public/css');
